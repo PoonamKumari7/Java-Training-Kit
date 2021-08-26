5ed6fdafa173ed72bd3e3c85a35a301aea35c7ac
Poonam Kumaari <88656326+PoonamKumari7@users.noreply.github.com>
2021-08-26 19:09:31 +0530

    Delete Day4 directory

--- Day4/OOPs/Person.java
+++
@@ -1,9 +0,0 @@
-package techment.Day4.OOPs;
-
-public class Person {
-	
-	int id;
-	String name;
-	int age;
-
-}
--- Day4/SuperVariable/Parent.java
+++
@@ -1,7 +0,0 @@
-package techment.Day4.SuperVariable;
-
-public class Parent {
-	
-	int b = 20;
-
-}
